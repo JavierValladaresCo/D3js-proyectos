a667dce0fa6e4a499b24dcbed6a4c37609e7d023
JavierValladaresCo <j.valladares.cortes@gmail.com>
2024-06-15 11:57:49 -0400

    Async function d3

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
-const x = d3.selectAll("#foo").style("color", "blue");
+async function label(d) {
+    return d
+}
 
-console.log(x)
+const data = [10, 20, 30, 40, 50]
+
+const el = d3.select('ul')
+    .selectAll('li')
+    .data(data)
+    .join('li')
+    .text(function (d) {
+        label(d).then(val => {
+            this.textContent = `El valor es ${val}`
+        })
+    });
+
