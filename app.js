23d5a429440ec95082567bd88c1738a2291f05d2
JavierValladaresCo <j.valladares.cortes@gmail.com>
2024-06-15 12:05:46 -0400

    enter, update, exit

--- app.js
+++ app.js
@@ -7,7 +7,14 @@ const data = [10, 20, 30, 40, 50]
 const el = d3.select('ul')
     .selectAll('li')
     .data(data)
-    .join('li')
+    .join(
+        enter => {
+            return enter.append('li')
+                .style('color', 'blue')
+        },
+        update => update.style('color', 'green'),
+        exit => exit.remove()
+    ) // retorna selecciones 
     .text(function (d) {
         label(d).then(val => {
             this.textContent = `El valor es ${val}`
